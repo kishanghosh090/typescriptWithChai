39259718e0dcab4ea72e70405c39b328c6c8a52c
kishanghosh090 <kishanghosh090@gmail.com>
2025-12-05 20:52:28 +0530

    typescript setup done

---
+++ setup/dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(name) {
+  return `hello ${name}, welcome to chai code`;
+}
+const username = greet("kishan rana ghosh");
+console.log(username);
+//# sourceMappingURL=index.js.map
---
+++ setup/src/index.ts
@@ -0,0 +1,6 @@
+function greet(name: String): String {
+  return `hello ${name}, welcome to chai code`;
+}
+
+const username: String = greet("kishan rana ghosh");
+console.log(username);
