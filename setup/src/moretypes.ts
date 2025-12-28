3ea4992bf721f43bae65797aeb97bfd0fbe37646
kishanghosh090 <kishanghosh090@gamil.com>
2025-12-28 19:08:05 +0530

    more on types

---
+++ setup/src/moretypes.ts
@@ -0,0 +1,33 @@
+let res: any = "56";
+let numeric: number = (res as string).length;
+type Book = {
+  name: string;
+};
+
+let bookString = '{"name":"chai"}';
+let bookObject = JSON.parse(bookString) as Book;
+console.log(bookObject.name);
+// type assertation
+const inputElement = document.getElementById("usname") as HTMLInputElement;
+try {
+  console.log(``);
+} catch (error: unknown) {
+  if (error instanceof Error) {
+    console.log(error.message);
+  } else {
+    console.log(error);
+  }
+}
+// never
+type Role = "admin" | "user";
+
+function redirectAsRole(role: Role): void {
+  if (role == "admin") {
+    console.log(`redirecting to admin dashboard`);
+  } else if (role == "user") {
+    console.log(`redirecting to user dashboard`);
+  }
+}
+function never() {
+  while (true) {}
+}
