925406de5ae236d515960dbd8d4d2f94bf12abdc
kishanghosh090 <kishanghosh090@gamil.com>
2025-12-28 22:09:23 +0530

    more on types

---
+++ setup/src/fun.ts
@@ -0,0 +1,9 @@
+function makeChai(type: string, cup: number) {
+  console.log(`making cups of ${type} quantity ${cup}`);
+}
+makeChai("masala", 12);
+
+function getChaiPrice(): number {
+  return 25;
+}
+getChaiPrice()
