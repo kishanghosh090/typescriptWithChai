a9aa001074244afa10e6c7d134b6a9c34ea191b5
kishanghosh090 <kishanghosh090@gamil.com>
2025-12-29 03:10:59 +0530

    oops1

--- setup/src/oops.ts
+++ setup/src/oops.ts
@@ -16,4 +16,49 @@ class Chai {
   revel(): string[] {
     return this.ing;
   }
+  protected shopName = "chai corner";
+  getShopName(): string {
+    return this.shopName;
+  }
+}
+
+class Masala extends Chai {
+  #balance = 100; //shop balance
+  override getShopName(): string {
+    return this.shopName;
+  }
+  getBalance() {
+    return this.#balance;
+  }
+}
+class ModernChai {
+  private _suger: number = 2;
+
+  public get getVal(): number {
+    return this._suger;
+  }
+
+  public set setVal(v: number) {
+    this._suger = v;
+  }
+}
+
+abstract class Drink {
+  abstract make(): void;
+}
+
+class MyChai extends Drink {
+  override make(): void {
+    console.log(`tea making... brewing...`);
+  }
+}
+
+class Heater {
+  heat() {}
+}
+class ChaiMaker {
+  constructor(private heater: Heater) {}
+  make() {
+    this.heater.heat;
+  }
 }
